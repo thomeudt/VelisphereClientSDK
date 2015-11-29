bd78dd1f0867e740845d8803d2a3a715a38345cb
thorsten <thorsten@thorsten-meudt.de>
2015-11-29 21:37:39 +0100

    fixed ssl error message

--- DemoLinuxScreenshotEngine/src/com/velisphere/demo/linuxScreenShots/PreDeployment.java
+++ DemoLinuxScreenshotEngine/src/com/velisphere/demo/linuxScreenShots/PreDeployment.java
@@ -6,7 +6,8 @@ public class PreDeployment {
 	
 	public static void initiateDeployment()
 	{
-		String provisioningID = ProvisioningClient.customProvisioning("unique2");
+		System.setProperty("jsse.enableSNIExtension", "false");
+		String provisioningID = ProvisioningClient.macProvisioning();
 		System.out.println("[IN] Provisioning request successfully submitted.");
 		System.out.println("[IN] Go to www.velisphere.com, log in with your user name and start the provisioning wizard to complete.");
 		System.out.println("[IN] Your Device ID is: " + provisioningID);
